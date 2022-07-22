bdc789a249daa817b293cf6d7634735ec9bb137b
Jonathan Krisna <65582909+abcdgick@users.noreply.github.com>
2022-07-22 15:30:40 +0700

    Add files via upload

---
+++ Vue Chat App/src/firebase.js
@@ -0,0 +1,64 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+import Filter from 'bad-words'
+import { ref, onUnmounted, computed } from 'vue'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAZ-K6p_fLQTxty3Mn9mGM23rHuunEXiDQ",
+  authDomain: "vital-cedar-355503.firebaseapp.com",
+  databaseURL: "https://vital-cedar-355503-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "vital-cedar-355503",
+  storageBucket: "vital-cedar-355503.appspot.com",
+  messagingSenderId: "941494569819",
+  appId: "1:941494569819:web:1d06def80bc96e88e1e87e",
+  measurementId: "G-4JVZ0YPTGN"
+})
+
+const auth = firebase.auth()
+
+export function useAuth() {
+  const user = ref(null)
+  const unsubscribe = auth.onAuthStateChanged(_user => (user.value = _user))
+  onUnmounted(unsubscribe)
+  const isLogin = computed(() => user.value !== null)
+
+  const signIn = async () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider()
+    await auth.signInWithPopup(googleProvider)
+  }
+  const signOut = () => auth.signOut()
+
+  return { user, isLogin, signIn, signOut }
+}
+
+const firestore = firebase.firestore()
+const messagesCollection = firestore.collection('messages')
+const messagesQuery = messagesCollection.orderBy('createdAt', 'desc').limit(100)
+const filter = new Filter()
+
+export function useChat() {
+  const messages = ref([])
+  const unsubscribe = messagesQuery.onSnapshot(snapshot => {
+    messages.value = snapshot.docs
+      .map(doc => ({ id: doc.id, ...doc.data() }))
+      .reverse()
+  })
+  onUnmounted(unsubscribe)
+
+  const { user, isLogin } = useAuth()
+  const sendMessage = text => {
+    if (!isLogin.value) return
+    const { photoURL, uid, displayName } = user.value
+    messagesCollection.add({
+      userName: displayName,
+      userId: uid,
+      userPhotoURL: photoURL,
+      text: filter.clean(text),
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    })
+  }
+
+  return { messages, sendMessage }
+}
